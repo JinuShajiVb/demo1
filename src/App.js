1b055a5eb53be1e5846990bc28b61e87098a0b76
JinuShajiVb <jinu@voxbaysolutions.com>
2022-08-11 16:42:37 +0530

    frist

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>This is our demo dashboard</h1>
+      <h2>welcome</h2>
+    </div>
+  );
+}
+
+export default App;
